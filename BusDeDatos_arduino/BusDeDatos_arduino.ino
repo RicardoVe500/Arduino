453f6808002ced28929dc3510b13d998ad7031fa
RicardoVe500 <ricardillo500@gmail.com>
2018-03-10 10:44:22 -0600

    Clase Finalizada

--- BusDeDatos_arduino/BusDeDatos_arduino.ino
+++ BusDeDatos_arduino/BusDeDatos_arduino.ino
@@ -1,17 +1,45 @@
+#define BuscarLed 1
+#define BuscarNumero 2
+
+int Led = 0;
+int Dato = 0;
+int EstadoBusqueda = 0;
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Hola Mundo");
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
  if(Serial.available()){
-   char Letra = Serial.read();
-   Serial.print(Letra);
-   if(Letra =='H'){
-    digitalWrite(13,1);
-   }
-   else if(Letra == 'L'){
-    digitalWrite(13,0);
-   }
+ char Letra = Serial.read();
+ if(Letra == 'L'){
+  EstadoBusqueda = BuscarLed;
+  }
+  else if(Letra == 'D'){
+    EstadoBusqueda = BuscarNumero;
+    }
+    else if(Letra == '.'){
+      analogWrite(Led, Dato);
+      Serial.print("El led es: ");
+      Serial.print(Led);
+      Serial.print(" El Dato es: ");
+      Serial.println(Dato);
+      Led = 0;
+      Dato = 0;
+      EstadoBusqueda = 0;
+      
+      }
+      else if(Letra >= '0' && Letra <= '9'){
+        int NumeroActual = int (Letra - '0');
+        if(EstadoBusqueda == BuscarLed){
+          Led = Led * 10 + NumeroActual;
+          }
+          else if(EstadoBusqueda == BuscarNumero){
+            Dato = Dato * 10 + NumeroActual;
+            }
+        }
  }
 }
