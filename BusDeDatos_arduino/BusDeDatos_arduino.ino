0089e4654a209d7eb0df0e9e81aab5ee15bee4b7
RicardoVe500 <ricardillo500@gmail.com>
2018-03-17 10:48:00 -0600

    boton

--- BusDeDatos_arduino/BusDeDatos_arduino.ino
+++ BusDeDatos_arduino/BusDeDatos_arduino.ino
@@ -4,15 +4,25 @@
 int Led = 0;
 int Dato = 0;
 int EstadoBusqueda = 0;
+int boton = 2;
 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hola Mundo");
   pinMode(13,OUTPUT);
+  pinMode(botob, INPUT);
 }
 
 void loop() {
+
+  //AQUI >>_______<<
+  if(digitalRead(boton) == 1){
+    Serial,print("H");
+    delay(500);
+    }
+
+    
  if(Serial.available()){
  char Letra = Serial.read();
  if(Letra == 'L'){
